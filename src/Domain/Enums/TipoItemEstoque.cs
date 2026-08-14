770278c7e26e0e6743df3563115a680ab6265059
Bea Tavernaro <94928593+beatavernaro@users.noreply.github.com>
2026-08-14 09:54:08 -0300

    Add ItemEstoque entity and validator with normalization and validation rules

---
+++ src/Domain/Enums/TipoItemEstoque.cs
@@ -0,0 +1,7 @@
+namespace Domain.Enums;
+
+public enum TipoItemEstoque
+{
+    Peca = 1,
+    Insumo = 2
+}
--- src/Domain/Validators/ClientValidator.cs
+++ src/Domain/Validators/ClientValidator.cs
@@ -1,6 +1,5 @@
 using Domain.Entities;
 using Domain.Enums;
-using Domain.ValueObjects;
 using Elekto.BrazilianDocuments;
 using FluentValidation;
 
